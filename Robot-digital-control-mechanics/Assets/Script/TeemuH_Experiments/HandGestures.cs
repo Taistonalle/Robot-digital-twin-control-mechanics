6e307e4636ed0b6e122c0080a31ceb7208c58cab
Teemu Homi <teemu.homi@netikka.fi>
2024-11-29 12:03:57 +0200

    Handscript changes.

--- Robot-digital-control-mechanics/Assets/Script/TeemuH_Experiments/HandGestures.cs
+++ Robot-digital-control-mechanics/Assets/Script/TeemuH_Experiments/HandGestures.cs
@@ -2,18 +2,20 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
-public class HandGestures: MonoBehaviour
+public class HandGestures : MonoBehaviour
 {
     bool stopped = true;
 
     [Header("Values")]
     [SerializeField][Range(0.1f, 5f)] float moveAmount;
     [SerializeField][Range(0.1f, 1f)] float loopWaitTime;
+    [SerializeField][Range(0.00001f, 0.001f)] float zThreshold = 0.0001f; // Lower threshold for small Z values
+    [SerializeField] float zScaleFactor = 1000f; // Scale factor to amplify small Z values
 
     [Header("Sliders")]
     [SerializeField] Slider X_pos_slider;
 
-    private float previousZ = 0f; // Store the Z value of index 0 from the previous frame
+    private float previousZ = 0f; // Store the scaled Z value of index 0 from the previous frame
 
     void Update()
     {
@@ -38,22 +40,32 @@ public class HandGestures: MonoBehaviour
         if (landmark0.gameObject.activeSelf)
         {
             Vector3 localPosition = landmark0.localPosition;
-            Debug.Log($"Index 0: Local Position X = {localPosition.x}, Y = {localPosition.y}, Z = {localPosition.z}");
 
-            // Check if Z value is increasing or decreasing
-            if (localPosition.z > previousZ)
-            {
-                if (!stopped) StopRoutine();
-                X_Up(); // Trigger X_Up function
-            }
-            else if (localPosition.z < previousZ)
-            {
-                if (!stopped) StopRoutine();
-                X_Down(); // Trigger X_Down function
-            }
+            // Scale the Z value to amplify small changes
+            float scaledZ = localPosition.z * zScaleFactor;
 
-            // Update previousZ for the next frame
-            previousZ = localPosition.z;
+            Debug.Log($"Index 0: Local Position X = {localPosition.x}, Y = {localPosition.y}, Z = {localPosition.z}, Scaled Z = {scaledZ}");
+
+            // Calculate the change in scaled Z
+            float zChange = scaledZ - previousZ;
+
+            // Check if the scaled Z change exceeds the threshold
+            if (Mathf.Abs(zChange) > zThreshold)
+            {
+                if (zChange > 0)
+                {
+                    if (!stopped) StopRoutine();
+                    X_Up(); // Trigger X_Up function
+                }
+                else if (zChange < 0)
+                {
+                    if (!stopped) StopRoutine();
+                    X_Down(); // Trigger X_Down function
+                }
+
+                // Update previousZ only if change exceeds the threshold
+                previousZ = scaledZ;
+            }
         }
         else
         {
@@ -65,6 +77,7 @@ public class HandGestures: MonoBehaviour
     void X_Up()
     {
         stopped = false;
+        Debug.Log("X is upping"); // Debug log for X_Up
         StartCoroutine(X_Up_Routine());
     }
 
@@ -72,6 +85,7 @@ public class HandGestures: MonoBehaviour
     void X_Down()
     {
         stopped = false;
+        Debug.Log("X is downing"); // Debug log for X_Down
         StartCoroutine(X_Down_Routine());
     }
 
